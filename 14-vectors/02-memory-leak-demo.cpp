a697bac3e300bb963cb47c73728de80d54b19dd9
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-26 20:41:18 +0530

    feat(vectors): demonstrate memory leak scenario with dynamic memory

---
+++ 14-vectors/02-memory-leak-demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int* func() {
+    int *ptr = new int;
+    *ptr = 1200;
+    cout << "ptr points to " << *ptr << endl;
+
+    return ptr;
+}
+
+int main() {
+    int *x = func();
+    cout << *x << endl;
+    return 0;
+}
